8ec68090fd908cad2a32727a21c194623244a5f8
muscleman <musclesonvacation@hotmail.com>
2022-05-23 20:10:55 -0500

    remove unused pipes and used angulars data pipe

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,46 +1,22 @@
 import { NgModule } from '@angular/core'
 import {
     BitNumberPipe,
-    DateFormatPipe,
     HashPowerConverterPipe,
     MoneyParsePipe,
-    OrderByPipe,
-    // OrderPipe,
-    SortByAlphabetPipe,
-    SortGridPipe,
-    TimeAgoPipe,
-    TimeAgoPipe2,
-    TruncatePipe,
-    UtcPipe
+    TimeAgoPipe2
 } from 'app/pipes/pipes.pipe'
 
 @NgModule({
     declarations: [
-        // OrderPipe,
-        OrderByPipe,
-        SortGridPipe,
         BitNumberPipe,
         MoneyParsePipe,
-        TruncatePipe,
-        TimeAgoPipe,
-        SortByAlphabetPipe,
         HashPowerConverterPipe,
-        DateFormatPipe,
-        UtcPipe,
         TimeAgoPipe2
     ],
     exports: [
-        // OrderPipe,
-        OrderByPipe,
-        SortGridPipe,
         BitNumberPipe,
         MoneyParsePipe,
-        TruncatePipe,
-        TimeAgoPipe,
-        SortByAlphabetPipe,
         HashPowerConverterPipe,
-        DateFormatPipe,
-        UtcPipe,
         TimeAgoPipe2
     ]
 })
